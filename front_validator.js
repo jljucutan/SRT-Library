1ab8473dce45c3f8aeec46bdaf70896a42d3094b
James L. Jucutan <jljucutan@gmail.com>
2018-07-03 07:30:11 +0800

    lists errors

--- front_validator.js
+++ front_validator.js
@@ -1,13 +1,11 @@
 var listErrors = function(errorArr) {
-    var list = $('<ul class="list-unstyled"/>');
+    var list = '';
     if (!errorArr.length) {
         return;
     }
-    list.html(
-        $.each(errorArr, function(k,v) {
-            $('<li/>').text(v);
-        });
-    );
+    $.each(errorArr, function(k,v) {
+        list += '<li>' + v + '</li>';
+    });
     return list;
 }
 
@@ -46,62 +44,60 @@ var validateField = function(field) {
         return true;
     }
     // validate input fields
-    else {
-        var isValid = true; 
-        var hasVal = field.val().length > 0;
-        var messages = [];
-        var d = new Date(),
-        var = fieldArr = field.val().split('/');
-        var fieldDate = new Date(fieldArr[2] + "-" + fieldArr[1] + "-" + fieldArr[0]);
+    var isValid = true; 
+    var hasVal = field.val().length > 0;
+    var messages = [];
+    var d = new Date();
+    var fieldArr = field.val().split('/');
+    var fieldDate = new Date(field.val());
 
-        // validate if required
-        if (isRequired && field.val().length < 1) {
-            message.push(fieldName + " is required.");
-            isValid = false;
-        }
-
-        // validate if date is past or future
-        if (hasVal && validations.indexOf('past-date') > -1 && fieldDate >= d) {
-            message.push(fieldName + " accepts only past date."); 
-            isValid =  false;
-        }
-        if (hasVal && validations.indexOf('future-date') > -1 && fieldDate < d) {
-            messages.push(fieldName + " accepts only future date.");
-            isValid =  false;
-        }
-
-        // validates and compares regex to value
-        if (hasVal && validations.indexOf('regex') > -1) {
-            var reg = new Regex(field.data('regex'));
-            if (false == reg.test(field.val())) {
-                messages.push(fieldName + " must match the constraint value");
-            }
-        }
-
-        // validates field length
-        if (hasVal && field.prop('minlength') && field.val() < field.prop('minlength')) {
-            messages.push(fieldName + " should not be less than " + field.prop('minlength') + "characters");
-        }
-        if (hasVal && field.prop('maxlength') && field.val() > field.prop('maxlength')) {
-            messages.push(fieldName + " should not exceed more than " + field.prop('maxlength') + "characters");
-        }
-
-        var errorContainer = $('[data-error-id="' + field.prop("name") + '"]');
-        var errorContainerTemplate = $('<span class="text-error" data-error-id="' + field.prop("name") + '" />');
-        var inputContainer = field.closest("div");
-
-        if (!isValid) {
-            field.addClass('input-error')
-            if (!errorContainer.length) {
-                inputContainer.append(errorContainerTemplate.html(listError(messages)).css({'display': 'inline-block'}));
-            } 
-            errorContainer.html(listError(messages)).css({'display': 'inline-block'});
-            return isValid;
-        } 
-        field.removeClass('input-error');
-        if (errorContainer.length) {
-            errorContainer.css({'display': 'none'});
-        } 
-        return isValid;
+    // validate if required
+    if (isRequired && field.val().length < 1) {
+        messages.push(fieldName + " is required.");
+        isValid = false;
     }
+
+    // validate if date is past or future
+    if (hasVal && validations.indexOf('past-date') > -1 && fieldDate >= d) {
+        messages.push(fieldName + " accepts only past date."); 
+        isValid =  false;
+    }
+    if (hasVal && validations.indexOf('future-date') > -1 && fieldDate < d) {
+        messages.push(fieldName + " accepts only future date.");
+        isValid =  false;
+    }
+
+    // validates and compares regex to value
+    if (hasVal && validations.indexOf('regex') > -1) {
+        var reg = new Regex(field.data('regex'));
+        if (false == reg.test(field.val())) {
+            messages.push(fieldName + " must match the constraint value");
+        }
+    }
+
+    // validates field length
+    if (hasVal && field.prop('minlength') && field.val() < field.prop('minlength')) {
+        messages.push(fieldName + " should not be less than " + field.prop('minlength') + "characters");
+    }
+    if (hasVal && field.prop('maxlength') && field.val() > field.prop('maxlength')) {
+        messages.push(fieldName + " should not exceed more than " + field.prop('maxlength') + "characters");
+    }
+
+    var errorContainer = $('[data-error-id="' + field.prop("name") + '"]');
+    var errorContainerTemplate = $('<ul class="list-unstyled text-error" data-error-id="' + field.prop("name") + '" />');
+    var inputContainer = field.closest("div");
+
+    if (!isValid) {
+        field.addClass('input-error')
+        if (!errorContainer.length) {
+            inputContainer.append(errorContainerTemplate.html(listErrors(messages)).css({'display': 'inline-block'}));
+        } 
+        errorContainer.html(listErrors(messages)).css({'display': 'inline-block'});
+        return isValid;
+    } 
+    field.removeClass('input-error');
+    if (errorContainer.length) {
+        errorContainer.css({'display': 'none'});
+    } 
+    return isValid;
 }
