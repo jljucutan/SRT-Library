78c6c622c8ba95a8a61ae92e49547334644d8235
James L. Jucutan <jljucutan@gmail.com>
2018-07-03 04:50:09 +0800

    added more validators and styling

--- front_validator.js
+++ front_validator.js
@@ -1,3 +1,16 @@
+var listErrors = function(errorArr) {
+    var list = $('<ul class="list-unstyled"/>');
+    if (!errorArr.length) {
+        return;
+    }
+    list.html(
+        $.each(errorArr, function(k,v) {
+            $('<li/>').text(v);
+        });
+    );
+    return list;
+}
+
 var validateField = function(field) {
     // please add data-title="field name" on form field
     var fieldName = field.data("title") ? field.data("title") : field.prop("title");
@@ -35,6 +48,7 @@ var validateField = function(field) {
     // validate input fields
     else {
         var isValid = true; 
+        var hasVal = field.val().length > 0;
         var messages = [];
         var d = new Date(),
         var = fieldArr = field.val().split('/');
@@ -42,21 +56,34 @@ var validateField = function(field) {
 
         // validate if required
         if (isRequired && field.val().length < 1) {
-            message = fieldName + " is required.";
+            message.push(fieldName + " is required.");
             isValid = false;
         }
 
         // validate if date is past or future
-        if (field.val().length > 0 && validations.indexOf('past-date') > -1 && fieldDate >= d) {
-            message = fieldName + " accepts only past date.";
+        if (hasVal && validations.indexOf('past-date') > -1 && fieldDate >= d) {
+            message.push(fieldName + " accepts only past date."); 
             isValid =  false;
         }
-        if (field.val().length > 0 && validations.indexOf('future-date') > -1 && fieldDate < d) {
-            message = fieldName + " accepts only future date.";
+        if (hasVal && validations.indexOf('future-date') > -1 && fieldDate < d) {
+            messages.push(fieldName + " accepts only future date.");
             isValid =  false;
         }
 
-        if (field.val().length > 0 && validations.indexOf('regex') > -1) {
+        // validates and compares regex to value
+        if (hasVal && validations.indexOf('regex') > -1) {
+            var reg = new Regex(field.data('regex'));
+            if (false == reg.test(field.val())) {
+                messages.push(fieldName + " must match the constraint value");
+            }
+        }
+
+        // validates field length
+        if (hasVal && field.prop('minlength') && field.val() < field.prop('minlength')) {
+            messages.push(fieldName + " should not be less than " + field.prop('minlength') + "characters");
+        }
+        if (hasVal && field.prop('maxlength') && field.val() > field.prop('maxlength')) {
+            messages.push(fieldName + " should not exceed more than " + field.prop('maxlength') + "characters");
         }
 
         var errorContainer = $('[data-error-id="' + field.prop("name") + '"]');
@@ -66,9 +93,9 @@ var validateField = function(field) {
         if (!isValid) {
             field.addClass('input-error')
             if (!errorContainer.length) {
-                inputContainer.append(errorContainerTemplate.html(message).css({'display': 'inline-block'}));
+                inputContainer.append(errorContainerTemplate.html(listError(messages)).css({'display': 'inline-block'}));
             } 
-            errorContainer.html(fieldName + " is required.").css({'display': 'inline-block'});
+            errorContainer.html(listError(messages)).css({'display': 'inline-block'});
             return isValid;
         } 
         field.removeClass('input-error');
